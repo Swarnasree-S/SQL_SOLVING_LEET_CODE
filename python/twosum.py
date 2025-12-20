32fe4a644105cc3be9ae59acde060524da6d13f9
Swarnasree-S <saiswarnasree@gmail.com>
2025-12-21 05:28:29 +0530

    Update twosum.py

--- python/twosum.py
+++ python/twosum.py
@@ -7,3 +7,30 @@ class Solution:
                   return [seen[need],i]
               seen[num]=i
   
+
+
+
+''''🔹 What does enumerate mean in Python?
+
+enumerate() lets you loop through a list and get both:
+
+Index (position)
+
+Value (element)
+
+👉 At the same time.
+🔹 Basic Example
+nums = [10, 20, 30]
+
+for i, num in enumerate(nums):
+    print(i, num)
+
+Output:
+0 10
+1 20
+2 30
+
+
+i → index (0, 1, 2)
+
+num → value (10, 20, 30)'''
